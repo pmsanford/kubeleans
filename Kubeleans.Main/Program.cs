46699ccdef99a5c3db06914d144bbd1f16b3164e
Paul Sanford <me@paulsanford.net>
2018-03-28 23:57:59 -0700

    Empty projects

---
+++ Kubeleans.Main/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kubeleans.Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
